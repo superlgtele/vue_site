a0ae83b1364c73c1dd91f7122dd5015c4400b611
superlgtele <superlgtele@naver.com>
2021-12-23 16:24:12 +0900

    Vue_for
    2021/12/23

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hi Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="(a,i) in menus" :key="i"> {{ a }} </a>
+  </div>
+
+  <div>
+    <h4 :style="styles">{{products[0]}}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>80 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 70,
+      styles : "color: blue",
+      products : ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      menus : ["Home", "Shop", "About"]
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,6 +44,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
