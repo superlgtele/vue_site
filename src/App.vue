6ded6010b985393ffda1e23cceac7185e35528b6
superlgtele <superlgtele@naver.com>
2021-12-25 12:44:02 +0900

    Import_Data
    2021/12/25

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,59 @@
     <a v-for="(a,i) in menus" :key="i"> {{ a }} </a>
   </div>
 
+  <div class="black-bg" v-if="Himodal == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>내용</p>
+      <button @click="Himodal = false">닫기</button>
+    </div>
+  </div>
+
   <div>
-    <h4 :style="styles">{{products[0]}}</h4>
-    <p>{{ price1 }} 만원</p>
-    <button @click="increase">허위매물신고</button> <span>신고수: {{report}}</span>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="Himodal = true" :style="styles"> {{products[0].title}} </h4>
+    <p>{{ products[0].price }}원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수: {{report[0]}}</span>
   </div>
   <div>
-    <h4>{{products[1]}}</h4>
-    <p>{{ price2 }} 만원</p>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4 :style="styles"> {{products[1].title}} </h4>
+    <p>{{ products[1].price }}원</p>
+    <button @click="increase1">허위매물신고</button> <span>신고수: {{report[1]}}</span>
   </div>
   <div>
-    <h4>{{products[2]}}</h4>
-    <p>80 만원</p>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4 :style="styles"> {{products[2].title}} </h4>
+    <p>{{ products[2].price }}원</p>
+    <button @click="increase2">허위매물신고</button> <span>신고수: {{report[2]}}</span>
   </div>
 </template>
 
 <script>
 
+import {roomdata} from "./assets/oneroom.js"
+
 export default {
   name: 'App',
   data(){
     return {
-      report : 0,
-      price1 : 60,
-      price2 : 70,
-      styles : "color: blue",
-      products : ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      menus : ["Home", "Shop", "About"]
+      report : [0,0,0],
+      styles : "color: green",
+      products : roomdata,
+      menus : ["Home", "Shop", "About"],
+      Himodal : false
     }
   },
 
   methods: {
     increase(){
-      this.report += 1;
+      this.report[0] += 1;
+    },
+    increase1(){
+      this.report[1] += 1;
+    },
+    increase2(){
+      this.report[2] += 1;
     }
   },
 
@@ -47,6 +67,26 @@ export default {
 </script>
 
 <style>
+body {
+  margin :0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
